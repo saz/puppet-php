2a3c47ae7cbddf81455f05f3ef9a4e074721bcf4
Saz <me@saz.sh>
2011-07-12 14:19:03 +0200

    Manage cli directory and symlink to conf.d

--- manifests/config.pp
+++ manifests/config.pp
@@ -27,4 +27,19 @@ class php::config {
         content => $php::cli_ini_content, 
         source  => $php::cli_ini_source,
     }
+
+    file { $php::params::cli_dir:
+        owner   => root,
+        group   => root,
+        purge   => true,
+        recurse => true,
+        force   => true,
+        require => Class["php::install"],
+        ensure  => directory,
+    }
+
+    file { "${php::params::cli_dir}conf.d":
+        ensure => "../conf.d",
+    }
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,8 @@ class php::params {
             $package_name = "php5-cli"
             $base_dir = "/etc/php5/"
             $cli_ini = "${base_dir}cli/php.ini"
-            $extra_dir = "${base_dir}extra"
+            $cli_dir = "${base_dir}cli/"
+            $extra_dir = "${base_dir}extra/"
             $conf_dir = "${base_dir}conf.d/"
         }
     }
