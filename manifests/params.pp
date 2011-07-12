666085d5f225888920678904c260f88430f24fdf
Saz <me@saz.sh>
2011-07-12 16:02:09 +0200

    Merge fpm/params.pp with params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,18 @@ class php::params {
         /(Ubuntu|Debian)/: {
             $package_name = "php5-cli"
             $base_dir = "/etc/php5/"
-            $cli_ini = "${base_dir}cli/php.ini"
             $cli_dir = "${base_dir}cli/"
+            $cli_ini = "${cli_dir}php.ini"
+            $cli_package_name = "php5-cli"
             $extra_dir = "${base_dir}extra/"
             $conf_dir = "${base_dir}conf.d/"
+            $fpm_dir = "${base_dir}fpm/"
+            $fpm_ini = "${fpm_dir}php.ini"
+            $fpm_package_name = "php5-fpm"
+            $fpm_conf = "${fpm_dir}php-fpm.conf"
+            $apache_dir = "${base_dir}apache2/"
+            $apache_ini = "${apache_dir}php.ini"
+            $apache_package_name = "libapache2-mod-php5"
         }
     }
 
