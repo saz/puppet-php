6604a1dcd982cf75300b7ff283ddd394eaa10fcc
Saz <me@saz.sh>
2011-07-13 01:16:16 +0200

    Cleanup for PHP running as module in apache

--- manifests/apache2/service.pp
+++
@@ -1,9 +0,0 @@
-class php::apache2::service {
-    service { $php::apache2::params::service_name:
-        ensure     => running,
-        hasstatus  => true,
-        hasrestart => true,
-        enable     => true,
-        require    => Class["php::apache2::install"],
-    }
-}
