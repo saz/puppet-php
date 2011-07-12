771fac3aef1c9f2a390cec74cd46f002ac299551
Saz <me@saz.sh>
2011-07-12 17:44:25 +0200

    Fixed FPM pool directory name

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,7 @@ class php::params {
             $extra_dir = "${base_dir}extra/"
             $conf_dir = "${base_dir}conf.d/"
             $fpm_dir = "${base_dir}fpm/"
-            $fpm_pool_dir = "${fpm_dir}pool/"
+            $fpm_pool_dir = "${fpm_dir}pool.d/"
             $fpm_ini = "${fpm_dir}php.ini"
             $fpm_package_name = "php5-fpm"
             $fpm_conf = "${fpm_dir}php-fpm.conf"
