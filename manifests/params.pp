10721564b7baba74a5f14c7ddb7141832c876523
Saz <me@saz.sh>
2011-07-12 12:01:57 +0200

    Remove commas at the end of lines

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,11 @@
 class php::params {
     case $operatingsystem {
         /(Ubuntu|Debian)/: {
-            $package_name = 'php5-cli',
-            $base_dir = '/etc/php5/',
-            $cli_ini = '${base_dir}cli/php.ini',
-            $extra_dir = '${base_dir}extra',
-            $conf_dir = '${base_dir}conf.d',
+            $package_name = 'php5-cli'
+            $base_dir = '/etc/php5/'
+            $cli_ini = '${base_dir}cli/php.ini'
+            $extra_dir = '${base_dir}extra'
+            $conf_dir = '${base_dir}conf.d'
         }
     }
 }
