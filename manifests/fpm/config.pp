3ef8893aeebb7997389d65634aef156df663bd1e
Saz <me@saz.sh>
2011-07-12 16:06:16 +0200

    Make file source and content configurable, merge fpm/params.pp with params.pp

--- manifests/fpm/config.pp
+++ manifests/fpm/config.pp
@@ -1,16 +1,45 @@
 class php::fpm::config {
-    file { $php::fpm::params::fpm_ini:
+    file { $php::params::fpm_dir:
+        owner   => root,
+        group   => root,
+        purge   => true,
+        recurse => true,
+        force   => true,
+        require => Class["php::install"],
+        ensure  => directory,
+    }
+
+    file { "${php::params::fpm_dir}conf.d":
+        ensure => "../conf.d",
+    }
+
+    file { "${php::params::fpm_dir}pool.d":
+        owner   => root,
+        group   => root,
+        purge   => true,
+        recurse => true,
+        force   => true,
+        require => Class["php::fpm::install"],
+        ensure  => directory,
+    }
+
+    file { $php::params::fpm_ini:
         owner   => root,
         group   => root,
         require => Class["php::fpm::install"],
         notify  => Class["php::fpm::service"],
         ensure  => file,
-        source  => [
-            "puppet:///files/hosts/${hostname}/php/fpm_php.ini",
-            "puppet:///files/hosts/${fqdn}/php/fpm_php.ini",
-            "puppet:///files/domains/${domain}/php/fpm_php.ini",
-            "puppet:///files/global/php/fpm_php.ini",
-            undef,
-        ],
+        content => $php::fpm::fpm_ini_content,
+        source  => $php::fpm::fpm_ini_source,
+    }
+
+    file { $php::params::fpm_conf:
+        owner   => root,
+        group   => root,
+        require => Class["php::fpm::install"],
+        notify  => Class["php::fpm::service"],
+        ensure  => file,
+        content => $php::fpm::fpm_conf_content,
+        source  => $php::fpm::fpm_conf_source,
     }
 }
--- manifests/fpm/init.pp
+++ manifests/fpm/init.pp
@@ -1,3 +1,3 @@
-class php::fpm {
-    include php::fpm::params, php::fpm::install, php::fpm::config, php::fpm::service
+class php::fpm($fpm_ini_content = undef, $fpm_ini_source = undef, $fpm_conf_content = undef, $fpm_conf_source = undef) {
+    include php::params, php::fpm::install, php::fpm::config, php::fpm::service
 }
