1f157a552633889c9b77b1cedd61a23643bc5f29
Saz <me@saz.sh>
2011-07-12 12:06:20 +0200

    Strings must be in double quotes if there are variables in the string

--- manifests/apache2/params.pp
+++ manifests/apache2/params.pp
@@ -3,9 +3,9 @@ class php::apache2::params {
 
     case $operatingsystem {
         /(Ubuntu|Debian)/: {
-            $package_name = 'libapache2-mod-php5'
-            $service_name = 'apache2'
-            $apache_ini = '${php::params::base_dir}apache2/php.ini'
+            $package_name = "libapache2-mod-php5"
+            $service_name = "apache2"
+            $apache_ini = "${php::params::base_dir}apache2/php.ini"
         }
     }
 }
--- manifests/fpm/params.pp
+++ manifests/fpm/params.pp
@@ -3,9 +3,9 @@ class php::fpm::params {
 
     case $operatingsystem {
         /(Ubuntu|Debian)/: {
-            $package_name = 'php5-fpm'
-            $service_name = 'php5-fpm'
-            $fpm_ini = '${php::params::base_dir}fpm/php.ini'
+            $package_name = "php5-fpm"
+            $service_name = "php5-fpm"
+            $fpm_ini = "${php::params::base_dir}fpm/php.ini"
         }
     }
 }
