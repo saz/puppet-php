d9fc36095af9288b6d60a679ef23871bd1d08891
Saz <me@saz.sh>
2011-07-12 15:48:04 +0200

    add class to manage files in extra directory

---
+++ manifests/extra.pp
@@ -0,0 +1,19 @@
+define php::extra($source = undef, $content = undef, $require = undef) {
+    # Puppet will bail out if both source and content is set,
+    # hence we don't have to deal with it.
+
+    file { "${name}":
+        path    => "${php::params::extra_dir}${name}.ini",
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        ensure  => present,
+        require => [
+            Class["php::config"],
+            $require,
+        ],
+        notify  => $php::params::notify,
+        source  => $source,
+        content => $content,
+    }
+}
