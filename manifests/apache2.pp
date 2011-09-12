7232991190277e26f03bd74f1f9ab946fee6f1af
Saz <me@saz.sh>
2011-09-12 12:16:00 +0200

    include php class

--- manifests/apache2.pp
+++ manifests/apache2.pp
@@ -1,6 +1,6 @@
 class php::apache2($apache2_ini_content = undef, $apache2_ini_source = undef) {
     require apache
-    include php::params, php::apache2::install, php::apache2::config
+    include php, php::apache2::install, php::apache2::config
 
     Class["php::config"] ~> Service["apache"]
 }
