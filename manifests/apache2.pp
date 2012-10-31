7117192f15f0ac5107a901a0ee3baad41118dddc
Steffen Zieger <me@saz.sh>
2012-10-31 01:57:24 +0100

    style guide fixes

--- manifests/apache2.pp
+++ manifests/apache2.pp
@@ -1,6 +1,11 @@
-class php::apache2($apache2_ini_content = undef, $apache2_ini_source = undef) {
+class php::apache2 (
+  $apache2_ini_content = undef,
+  $apache2_ini_source = undef
+) inherits php::params {
     require apache
-    include php, php::apache2::install, php::apache2::config
+    include php
+    include php::apache2::install
+    include php::apache2::config
 
-    Class["php::config"] ~> Service["apache"]
+    Class['php::config'] ~> Service[$php::params::apache_service_name]
 }
--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -1,5 +1,13 @@
-class php::fpm($fpm_ini_content = undef, $fpm_ini_source = undef, $fpm_conf_content = undef, $fpm_conf_source = undef) {
-    include php, php::fpm::install, php::fpm::config, php::fpm::service
+class php::fpm (
+  $fpm_ini_content = undef,
+  $fpm_ini_source = undef,
+  $fpm_conf_content = undef,
+  $fpm_conf_source = undef
+) {
+  include php
+  include php::fpm::install
+  include php::fpm::config
+  include php::fpm::service
 
-    Class["php::config"] ~> Class["php::fpm::service"]
+  Class['php::config'] ~> Class['php::fpm::service']
 }
