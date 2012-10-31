2f0f94de73cefd22a27a2995350f48642f7aaca2
Steffen Zieger <me@saz.sh>
2012-10-31 02:02:31 +0100

    more style guide fixes

--- manifests/extra.pp
+++ manifests/extra.pp
@@ -1,33 +1,43 @@
-define php::extra($ensure = present, $source = undef, $content = undef, $require = undef, $notify = undef) {
-    # Puppet will bail out if both source and content is set,
-    # hence we don't have to deal with it.
+define php::extra (
+  $ensure = present,
+  $source = undef,
+  $content = undef,
+  $require = undef,
+  $notify = undef
+) {
+  # Puppet will bail out if both source and content is set,
+  # hence we don't have to deal with it.
 
-    $file_name = "${name}.ini"
+  $file_name = "${name}.ini"
 
-    file { $file_name:
-        path    => "${php::params::extra_dir}${file_name}",
-        mode    => 644,
-        owner   => root,
-        group   => root,
-        ensure  => $ensure,
-        notify  => $notify,
-        require => [
-            Class["php"],
-            $require,
-        ],
-        source  => $source ? {
-            undef   => undef,
-            true    => [
-                "puppet:///files/${fqdn}/etc/php5/extra/${file_name}",
-                "puppet:///files/${hostgroup}/etc/php5/extra/${file_name}",
-                "puppet:///files/${domain}/etc/php5/extra/${file_name}",
-                "puppet:///files/global/etc/php5/extra/${file_name}",
-            ],
-            default => "${source}${file_name}",
-        },
-        content => $content ? {
-            undef   => undef,
-            default => template("${content}${file_name}.erb"),
-        },
-    }
+  $source_real = $source ? {
+    default => "${source}${file_name}",
+    undef   => undef,
+    true    => [
+      "puppet:///files/${::fqdn}/etc/php5/extra/${file_name}",
+      "puppet:///files/${::hostgroup}/etc/php5/extra/${file_name}",
+      "puppet:///files/${::domain}/etc/php5/extra/${file_name}",
+      "puppet:///files/global/etc/php5/extra/${file_name}",
+    ],
+  }
+
+  $content_real = $content ? {
+    undef   => undef,
+    default => template("${content}${file_name}.erb"),
+  }
+
+  file { $file_name:
+    ensure  => $ensure,
+    path    => "${php::params::extra_dir}${file_name}",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    notify  => $notify,
+    source  => $source_real,
+    content => $content_real,
+    require => [
+      Class['php'],
+      $require,
+    ],
+  }
 }
--- manifests/module.pp
+++ manifests/module.pp
@@ -1,44 +1,58 @@
-define php::module($ensure = present, $source = undef, $content = undef, $require = undef, $notify = undef, $package_prefix = 'php5-') {
-    include php
+define php::module(
+  $ensure = present,
+  $package_prefix = 'php5-',
+  $source = undef,
+  $content = undef,
+  $require = undef,
+  $notify = undef
+) {
+  include php
 
-    $file_name = "${name}.ini"
+  $file_name = "${name}.ini"
 
-    if $require {
-      $real_require = [ Class['php::install'], $require, ]
-    } else {
-      $real_require = Class['php::install']
-    }
+  if $require {
+    $real_require = [
+      Class['php::install'],
+      $require,
+    ]
+  } else {
+    $real_require = Class['php::install']
+  }
 
-    package { "php-${name}":
-        name => "${package_prefix}${name}",
-        ensure  => $ensure,
-        require => $real_require,
-    }
+  package { "php-${name}":
+    ensure  => $ensure,
+    name    => "${package_prefix}${name}",
+    require => $real_require,
+  }
 
-    file { $file_name:
-        path    => "${php::params::conf_dir}${file_name}",
-        mode    => 644,
-        owner   => root,
-        group   => root,
-        ensure  => $ensure,
-        notify  => $notify,
-        source  => $source ? {
-            undef   => undef,
-            true    => [
-                "puppet:///files/${fqdn}/etc/php5/conf.d/${file_name}",
-                "puppet:///files/${hostgroup}/etc/php5/conf.d/${file_name}",
-                "puppet:///files/${domain}/etc/php5/conf.d/${file_name}",
-                "puppet:///files/global/etc/php5/conf.d/${file_name}",
-            ], 
-            default => "${source}${file_name}",
-        },
-        content => $content ? {
-            undef   => undef,
-            default => template("${content}${file_name}.erb"),
-        },
-        require => [
-            Class["php"],
-            Package["php-${name}"],
-        ],
-    }
+  $source_real = $source ? {
+    default => "${source}${file_name}",
+    undef   => undef,
+    true    => [
+      "puppet:///files/${::fqdn}/etc/php5/conf.d/${file_name}",
+      "puppet:///files/${::hostgroup}/etc/php5/conf.d/${file_name}",
+      "puppet:///files/${::domain}/etc/php5/conf.d/${file_name}",
+      "puppet:///files/global/etc/php5/conf.d/${file_name}",
+    ],
+  }
+
+  $content_real = $content ? {
+    undef   => undef,
+    default => template("${content}${file_name}.erb"),
+  }
+
+  file { $file_name:
+    ensure  => $ensure,
+    path    => "${php::params::conf_dir}${file_name}",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    notify  => $notify,
+    source  => $source_real,
+    content => $content_real,
+    require => [
+      Class['php'],
+      Package["php-${name}"],
+    ],
+  }
 }
